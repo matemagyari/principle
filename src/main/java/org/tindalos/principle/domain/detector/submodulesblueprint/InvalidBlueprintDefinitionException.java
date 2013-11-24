8609f0c8f110c62de7b5a4dac8856ec6b5904e33
unknown <mate.magyari@gmail.com>
2013-11-24 12:44:21 +0000

    ref

--- src/main/java/org/tindalos/principle/domain/detector/submodulesblueprint/InvalidBlueprintDefinitionException.java
+++ src/main/java/org/tindalos/principle/domain/detector/submodulesblueprint/InvalidBlueprintDefinitionException.java
@@ -1,12 +1,11 @@
 package org.tindalos.principle.domain.detector.submodulesblueprint;
 
+import org.tindalos.principle.domain.expectations.exception.InvalidConfigurationException;
+
 @SuppressWarnings("serial")
-public class InvalidBlueprintDefinitionException extends RuntimeException {
+public class InvalidBlueprintDefinitionException extends InvalidConfigurationException {
 
 	public InvalidBlueprintDefinitionException(String msg) {
 		super(msg);
 	}
-	
-	
-
 }
