4983fc302e85add271bd1f832b0c42576e60ef51
Mate Magyari <Mate.Magyari@Gamesys.co.uk>
2013-12-18 17:19:59 +0000

    SDPTest

---
+++ src/main/java/org/tindalos/principletest/sdp/stable3/Test3.java
@@ -0,0 +1,11 @@
+package org.tindalos.principletest.sdp.stable3;
+
+import org.tindalos.principletest.sdp.stable1.Test1;
+import org.tindalos.principletest.sdp.stable2.Test2;
+
+public class Test3 {
+    
+    Test2 test2;
+    Test1 test1; 
+
+}
