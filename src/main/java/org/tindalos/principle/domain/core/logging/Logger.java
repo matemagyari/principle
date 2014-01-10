c6b73c39aa3d2b9ca841809e097f5277bece409d
Mate Magyari <Mate.Magyari@Gamesys.co.uk>
2014-01-10 17:40:04 +0000

    scalafy

--- src/main/java/org/tindalos/principle/domain/core/logging/Logger.java
+++
@@ -1,8 +0,0 @@
-package org.tindalos.principle.domain.core.logging;
-
-public interface Logger {
-    
-    void info(String msg);
-    void error(String msg);
-
-}
--- src/main/java/org/tindalos/principle/domain/resultprocessing/reporter/Printer.java
+++
@@ -1,8 +0,0 @@
-package org.tindalos.principle.domain.resultprocessing.reporter;
-
-public interface Printer {
-	
-	void printInfo(String text);
-	void printWarning(String text);
-
-}
