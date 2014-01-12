048d0cfc928d6d51fa1178b7419edd03ff00d244
unknown <mate.magyari@gmail.com>
2014-01-12 18:57:47 +0000

    scalafy

---
+++ src/main/java/org/tindalos/principle/domain/core/Cycle.scala
@@ -0,0 +1,49 @@
+package org.tindalos.principle.domain.core
+
+import org.apache.commons.lang3.builder.HashCodeBuilder
+
+class Cycle(val references: List[PackageReference]) extends Comparable[Cycle] {
+  
+  def this(refs:java.util.List[PackageReference]) = this(ListConverter.convert(refs))
+  def this(refs:PackageReference*) = this(refs.toList)
+  def this(ref1:PackageReference,ref2:PackageReference) = this(List(ref1, ref2))
+  def this(ref1:PackageReference,ref2:PackageReference,ref3:PackageReference) = this(List(ref1, ref2, ref3))
+
+  if (references == null || references.length < 2) throw new DomainException("Invalid cycle " + references)
+
+  def getLast() = references.last
+  
+  def getReferences():java.util.List[PackageReference] = ListConverter.convert(references)
+
+  override def toString() = {
+    val arrow = "-->"
+    val sb = new StringBuffer("*" + arrow)
+    references foreach { reference =>
+      sb.append(reference + arrow)
+    }
+    sb.append("*").toString()
+  }
+
+  override def equals(other: Any) = other match {
+    case castOther: Cycle =>
+      if (!references.toSet.equals(castOther.references.toSet)) false
+      else {
+
+        val offset = castOther.references.indexOf(references.head)
+        var result = true
+        for (i <- 0 to references.length-1 if result) {
+          val indexWithOffset = (i + offset) % references.length
+          if (!references(i).equals(castOther.references(indexWithOffset))) {
+            result = false
+          }
+        }
+        result
+      }
+    case _ => false
+  }
+
+  override def hashCode() = new HashCodeBuilder().append(references.length).hashCode()
+  
+  override def compareTo(that:Cycle) = this.toString().compareTo(that.toString())
+
+}
