1f7f7ab34485b93d8170e1b9519edbb2b4b8e42a
Mate Magyari <Mate.Magyari@Gamesys.co.uk>
2014-02-06 18:12:46 +0000

    report

---
+++ src/main/java/org/tindalos/principle/app/service/MyApp.scala
@@ -0,0 +1,13 @@
+package org.tindalos.principle.app.service
+
+object MyApp extends App {
+  
+  val x = Some("Bela")
+  
+  val li = List(1,2)
+  
+  
+  println(li.map(_*2))
+  //x.flatMap(x => x.+x)
+
+}
