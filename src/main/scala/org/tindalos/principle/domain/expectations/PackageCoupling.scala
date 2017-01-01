65547997e40c3d15675665b2b758c65eb80cd1b4
matemagyari <mate.magyari@gmail.com>
2017-01-01 22:11:19 +0100

    [remove_nulls] remove nulls - 4

--- src/main/scala/org/tindalos/principle/domain/expectations/PackageCoupling.scala
+++ src/main/scala/org/tindalos/principle/domain/expectations/PackageCoupling.scala
@@ -1,16 +1,15 @@
 package org.tindalos.principle.domain.expectations
 
-class PackageCoupling {
+//todo - refactor it to a case class
+class PackageCoupling(
+  var adp: ADP = null,
+  var sdp: SDP = null,
+  var sap: SAP = null,
 
-  var adp: ADP = null
-  var sdp: SDP = null
-  var sap: SAP = null
+  var acd: ACD = null,
+  var racd: RACD = null,
+  var nccd: NCCD = null,
 
-  var acd: ACD = null
-  var racd: RACD = null
-  var nccd: NCCD = null
-
-  var grouping: Grouping = null
-}
+  var grouping: Grouping = null)
 
 case class Grouping()
