16798bb10f087e2dc03d90c6a86d34c2f420a648
magma <mate.magyari@leonteq.com>
2026-02-16 20:20:54 +0100

    javafy SubmoduleId

---
+++ src/main/java/org/tindalos/principle/domain/analyzers/submodulesblueprint/SubmoduleId.java
@@ -0,0 +1,10 @@
+package org.tindalos.principle.domain.analyzers.submodulesblueprint;
+
+/**
+ * Identifier for a submodule in the blueprint definition.
+ *
+ * @param value the string identifier of the submodule
+ */
+public record SubmoduleId(String value) {
+}
+
