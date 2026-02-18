6f45f7413808272f79b3cdf8626f6de302e1ef96
magma <mate.magyari@leonteq.com>
2026-02-18 16:48:16 +0100

    improve YAMLBasedSubmodulesBlueprintProviderTest

--- src/test/scala/org/tindalos/principle/infrastructure/analyzers/submodulesblueprint/YAMLBasedSubmodulesBlueprintProviderTest.scala
+++ src/test/scala/org/tindalos/principle/infrastructure/analyzers/submodulesblueprint/YAMLBasedSubmodulesBlueprintProviderTest.scala
@@ -3,10 +3,31 @@ package org.tindalos.principle.infrastructure.analyzers.submodulesblueprint
 import org.junit.Assert._
 import org.junit._
 import org.tindalos.principle.domain.analyzers.submodulesblueprint._
+
 import java.io.File
+import org.tindalos.principle.domain.core.PackageReference
+
+import java.util.{Collections, Set}
 import org.apache.commons.io.FileUtils
 
+import java.util
+
 class YAMLBasedSubmodulesBlueprintProviderTest {
+
+  val yamlFile = createTempYamlFile(
+    """
+      |checks:
+      |  modules:
+      |    module-definitions:
+      |      MOD1: [domain.mod1, app.mod1]
+      |      MOD2: [domain.mod2, app.mod2]
+      |      MOD3: [domain.mod3, app.mod3]
+      |    module-dependencies:
+      |      MOD1: [MOD2, MOD3]
+      |      MOD2: [MOD3]
+      |      MOD3: []
+      |    violation_threshold: 0
+    """.stripMargin)
 
   private def createTempYamlFile(content: String): String = {
     val tempFile = File.createTempFile("test_blueprint_", ".yaml")
@@ -17,267 +38,95 @@ class YAMLBasedSubmodulesBlueprintProviderTest {
 
   @Test
   def readSubmoduleDefinitions_validYaml_parsesSuccessfully(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |      MOD3: [domain.mod3, app.mod3]
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |      MOD2: [MOD1]
-      |      MOD3: []
-      |    violation_threshold: 0
-    """.stripMargin
 
-    val yamlFile = createTempYamlFile(yaml)
     val result = YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
       yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
+      "com"
     )
 
     assertNotNull("SubmoduleDefinitions should not be null", result)
-    val definitions = result.getDefinitions()
-    assertEquals("Should have 3 module definitions", 3, definitions.size())
-  }
 
-  @Test
-  def readSubmoduleDefinitions_validYaml_containsExpectedModules(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |      MOD3: [domain.mod3, app.mod3]
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |      MOD2: [MOD1]
-      |      MOD3: []
-      |    violation_threshold: 0
-    """.stripMargin
+    val definitions: util.Map[SubmoduleId, SubmoduleDefinition] = result.getDefinitions()
+    assertEquals(3, definitions.size())
+    assertEquals(util.Set.of(
+      new SubmoduleId("MOD1"),
+      new SubmoduleId("MOD2"),
+      new SubmoduleId("MOD3")), definitions.keySet())
 
-    val yamlFile = createTempYamlFile(yaml)
-    val result = YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
-      yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
-    )
 
-    val definitions = result.getDefinitions()
+    val definition1: SubmoduleDefinition = definitions.get(new SubmoduleId("MOD1"))
+    assertEquals(util.Set.of(new SubmoduleId("MOD2"), new SubmoduleId("MOD3")), definition1.getLegalDependencies())
+    assertEquals(util.Set.of(new PackageReference("com.domain.mod1"), new PackageReference("com.app.mod1")), definition1.packages())
 
-    // Verify MOD1 exists
-    val mod1Id = new SubmoduleId("MOD1")
-    assertTrue("Should contain MOD1", definitions.containsKey(mod1Id))
+    val definition2: SubmoduleDefinition = definitions.get(new SubmoduleId("MOD2"))
+    assertEquals(Collections.singleton(new SubmoduleId("MOD3")), definition2.getLegalDependencies())
+    assertEquals(util.Set.of(new PackageReference("com.domain.mod2"), new PackageReference("com.app.mod2")), definition2.packages())
 
-    // Verify MOD2 exists
-    val mod2Id = new SubmoduleId("MOD2")
-    assertTrue("Should contain MOD2", definitions.containsKey(mod2Id))
+    val definition3: SubmoduleDefinition = definitions.get(new SubmoduleId("MOD3"))
+    assertTrue(definition3.getLegalDependencies().isEmpty)
+    assertEquals(util.Set.of(new PackageReference("com.domain.mod3"), new PackageReference("com.app.mod3")), definition3.packages())
 
-    // Verify MOD3 exists
-    val mod3Id = new SubmoduleId("MOD3")
-    assertTrue("Should contain MOD3", definitions.containsKey(mod3Id))
-  }
-
-  @Test
-  def readSubmoduleDefinitions_validYaml_modulesHaveCorrectPackages(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |      MOD3: [domain.mod3, app.mod3]
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |      MOD2: [MOD1]
-      |      MOD3: []
-      |    violation_threshold: 0
-    """.stripMargin
-
-    val yamlFile = createTempYamlFile(yaml)
-    val result = YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
-      yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
-    )
-
-    val definitions = result.getDefinitions()
-    val mod1 = definitions.get(new SubmoduleId("MOD1"))
-
-    assertNotNull("MOD1 should exist", mod1)
-    val packages = mod1.packages()
-    assertEquals("MOD1 should have 2 packages", 2, packages.size())
-
-    import scala.collection.JavaConverters._
-    val packageNames = packages.asScala.map(_.toString).toSet
-    assertTrue("Should contain domain.mod1 package",
-      packageNames.exists(_.contains("domain.mod1")))
-    assertTrue("Should contain app.mod1 package",
-      packageNames.exists(_.contains("app.mod1")))
-  }
-
-  @Test
-  def readSubmoduleDefinitions_validYaml_modulesHaveCorrectDependencies(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |      MOD3: [domain.mod3, app.mod3]
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |      MOD2: [MOD1]
-      |      MOD3: []
-      |    violation_threshold: 0
-    """.stripMargin
-
-    val yamlFile = createTempYamlFile(yaml)
-    val result = YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
-      yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
-    )
-
-    val definitions = result.getDefinitions()
-    val mod1 = definitions.get(new SubmoduleId("MOD1"))
-    val mod2 = definitions.get(new SubmoduleId("MOD2"))
-
-    // MOD1 should depend on MOD2
-    import scala.collection.JavaConverters._
-    val mod1Deps = mod1.getLegalDependencies().asScala
-    assertTrue("MOD1 should depend on MOD2",
-      mod1Deps.exists(_.value == "MOD2"))
-
-    // MOD2 should depend on MOD1
-    val mod2Deps = mod2.getLegalDependencies().asScala
-    assertTrue("MOD2 should depend on MOD1",
-      mod2Deps.exists(_.value == "MOD1"))
-  }
-
-  @Test
-  def readSubmoduleDefinitions_validYaml_mod3HasNoDependencies(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |      MOD3: [domain.mod3, app.mod3]
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |      MOD2: [MOD1]
-      |      MOD3: []
-      |    violation_threshold: 0
-    """.stripMargin
-
-    val yamlFile = createTempYamlFile(yaml)
-    val result = YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
-      yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
-    )
-
-    val definitions = result.getDefinitions()
-    val mod3 = definitions.get(new SubmoduleId("MOD3"))
-
-    val mod3Deps = mod3.getLegalDependencies()
-    assertEquals("MOD3 should have no dependencies", 0, mod3Deps.size())
   }
 
   @Test(expected = classOf[InvalidBlueprintDefinitionException])
   def readSubmoduleDefinitions_missingFile_throwsException(): Unit = {
     YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
       "src/test/resources/non_existent_file.yaml",
-      "org.tindalos.principletest.submodulesblueprint"
+      "com"
     )
   }
 
   @Test(expected = classOf[InvalidBlueprintDefinitionException])
   def readSubmoduleDefinitions_missingModuleDefinitions_throwsException(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |    violation_threshold: 0
-    """.stripMargin
-
-    val yamlFile = createTempYamlFile(yaml)
+    val yamlFile = createTempYamlFile("""
+                                        |checks:
+                                        |  modules:
+                                        |    module-dependencies:
+                                        |      MOD1: [MOD2]
+                                        |    violation_threshold: 0
+    """.stripMargin)
     YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
       yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
+      "com"
     )
   }
 
   @Test(expected = classOf[InvalidBlueprintDefinitionException])
   def readSubmoduleDefinitions_missingModuleDependencies_throwsException(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |    violation_threshold: 0
+    val yaml =
+      """
+        |checks:
+        |  modules:
+        |    module-definitions:
+        |      MOD1: [domain.mod1, app.mod1]
+        |      MOD2: [domain.mod2, app.mod2]
+        |    violation_threshold: 0
     """.stripMargin
 
     val yamlFile = createTempYamlFile(yaml)
     YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
       yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
+      "com"
     )
   }
 
   @Test(expected = classOf[OverlappingSubmoduleDefinitionsException])
   def readSubmoduleDefinitions_overlappingModules_throwsException(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain]
-      |      MOD2: [domain.mod2]
-      |    module-dependencies:
-      |      MOD1: []
-      |      MOD2: []
-      |    violation_threshold: 0
-    """.stripMargin
-
-    val yamlFile = createTempYamlFile(yaml)
+    val yamlFile = createTempYamlFile("""
+                                        |checks:
+                                        |  modules:
+                                        |    module-definitions:
+                                        |      MOD1: [domain]
+                                        |      MOD2: [domain.mod2]
+                                        |    module-dependencies:
+                                        |      MOD1: []
+                                        |      MOD2: []
+                                        |    violation_threshold: 0
+    """.stripMargin)
     YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
       yamlFile,
       "org.tindalos.principletest.submodulesblueprint"
     )
-  }
-
-  @Test
-  def readSubmoduleDefinitions_basePackageNamePrependedCorrectly(): Unit = {
-    val yaml = """
-      |checks:
-      |  modules:
-      |    module-definitions:
-      |      MOD1: [domain.mod1, app.mod1]
-      |      MOD2: [domain.mod2, app.mod2]
-      |      MOD3: [domain.mod3, app.mod3]
-      |    module-dependencies:
-      |      MOD1: [MOD2]
-      |      MOD2: [MOD1]
-      |      MOD3: []
-      |    violation_threshold: 0
-    """.stripMargin
-
-    val yamlFile = createTempYamlFile(yaml)
-    val result = YAMLBasedSubmodulesBlueprintProvider.readSubmoduleDefinitions(
-      yamlFile,
-      "org.tindalos.principletest.submodulesblueprint"
-    )
-
-    val definitions = result.getDefinitions()
-    val mod1 = definitions.get(new SubmoduleId("MOD1"))
-
-    import scala.collection.JavaConverters._
-    val packageRefs = mod1.packages().asScala
-
-    // Verify all packages start with the base package name
-    assertTrue("All packages should start with base package name",
-      packageRefs.forall(_.toString.startsWith("org.tindalos.principletest.submodulesblueprint")))
   }
 }
 
