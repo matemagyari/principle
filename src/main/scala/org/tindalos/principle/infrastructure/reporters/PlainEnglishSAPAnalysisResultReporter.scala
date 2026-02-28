69abac20660db6d512041851fe142526be95f974
magma <mate.magyari@leonteq.com>
2026-02-28 23:29:37 +0100

    add reporters

---
+++ src/main/scala/org/tindalos/principle/infrastructure/reporters/PlainEnglishSAPAnalysisResultReporter.scala
@@ -0,0 +1,29 @@
+package org.tindalos.principle.infrastructure.reporters
+
+import org.tindalos.principle.domain.core.Package
+import org.tindalos.principle.domain.analyzers.sap.SAPResult
+import org.tindalos.principle.domain.resultprocessing.reporter.AnalysisResultsReporter
+
+class PlainEnglishSAPAnalysisResultReporter extends SAPAnalysisResultReporter {
+
+  def report(result: SAPResult):AnalysisResultsReporter.Report = {
+    val outlierPackages = result.outlierPackages
+    val sectionLine = "=============================================================="
+    val sb = new StringBuffer("\n" + sectionLine + "\n")
+    sb.append("\tStable Abstractions Principle violations (" + outlierPackages.length + " of allowed " + result.threshold + " )\t")
+    sb.append("\n" + sectionLine + "\n")
+
+    if (outlierPackages.isEmpty) {
+      sb.append("No violations.\n")
+    } else {
+      outlierPackages foreach { outlierPackage =>
+        sb.append(print(outlierPackage) + "\n")
+      }
+    }
+    sb.append(sectionLine + "\n")
+    sb.toString()
+  }
+
+  private def print(outlierPackage: Package) = outlierPackage + " " + outlierPackage.distance
+
+}
---
+++ src/main/scala/org/tindalos/principle/infrastructure/reporters/PlainEnglishSDPAnalysisResultReporter.scala
@@ -0,0 +1,33 @@
+package org.tindalos.principle.infrastructure.reporters
+
+import org.tindalos.principle.domain.analyzers.sdp.{SDPViolation, SDPResult}
+import org.tindalos.principle.domain.resultprocessing.reporter.AnalysisResultsReporter
+
+class PlainEnglishSDPAnalysisResultReporter extends SDPAnalysisResultReporter {
+
+  def report(result: SDPResult):AnalysisResultsReporter.Report = {
+    val violations = result.violations
+    val sectionLine = "=============================================================="
+    val sb = new StringBuffer("\n" + sectionLine + "\n")
+    sb.append("\tStable Dependencies Principle violations (" + violations.length + " of allowed " + result.threshold + " )\t")
+    sb.append("\n" + sectionLine + "\n")
+
+    if (violations.isEmpty)
+      sb.append("No violations.\n")
+    else
+      violations foreach { violation => sb.append(print(violation) + "\n")}
+    sb.append(sectionLine + "\n")
+    sb.toString()
+  }
+
+  private def print(violation: SDPViolation) = {
+
+    val arrow = "-->"
+    val sb = new StringBuffer("")
+    sb.append("\n " + violation.depender.reference + "[" + violation.depender.instability + "] ")
+    sb.append(arrow)
+    sb.append(" " + violation.dependee.reference + "[" + violation.dependee.instability + "] ")
+    sb.toString()
+  }
+
+}
