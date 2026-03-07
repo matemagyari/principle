e8e9b83b94fea836666db731e34931f7c0d6ca2c
magma <mate.magyari@leonteq.com>
2026-03-07 22:07:00 +0100

    move SDPAnalysisResultReporter to Java

---
+++ src/main/java/org/tindalos/principle/app/reporters/SDPAnalysisResultReporter.java
@@ -0,0 +1,7 @@
+package org.tindalos.principle.app.reporters;
+
+import org.tindalos.principle.domain.analyzers.sdp.SDPResult;
+
+public interface SDPAnalysisResultReporter extends AnalysisResultReporter<SDPResult> {
+}
+
--- src/main/scala/org/tindalos/principle/app/reporters/SDPAnalysisResultReporter.scala
+++
@@ -1,6 +0,0 @@
-package org.tindalos.principle.app.reporters
-
-import org.tindalos.principle.domain.analyzers.sdp.SDPResult
-
-trait SDPAnalysisResultReporter extends AnalysisResultReporter[SDPResult]
-
