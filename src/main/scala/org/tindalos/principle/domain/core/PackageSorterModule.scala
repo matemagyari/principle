99725879a929a5ddc224f73afc4dab3fedda6ba0
magma <mate.magyari@leonteq.com>
2026-03-14 22:15:03 +0100

    change PackageSorterModule

--- src/main/scala/org/tindalos/principle/domain/core/PackageSorterModule.scala
+++ src/main/scala/org/tindalos/principle/domain/core/PackageSorterModule.scala
@@ -1,10 +1,12 @@
 package org.tindalos.principle.domain.core
 
+import org.tindalos.principle.domain.core.packages.PackageWithMetrics
+
 object PackageSorterModule {
   
-  def sortByName(packages:List[Package], basePackageName:String):List[Package] = 
+  def sortByName[T <: PackageWithMetrics](packages:List[T], basePackageName:String):List[T] =
 	 sortByName(packages).filter(_.reference.startsWith(basePackageName))
   
-  def sortByName(packages:List[Package]): List[Package] = packages.sortBy(_.reference.name)
+  def sortByName[T <: PackageWithMetrics](packages:List[T]): List[T] = packages.sortBy(_.reference.name)
 
 }
