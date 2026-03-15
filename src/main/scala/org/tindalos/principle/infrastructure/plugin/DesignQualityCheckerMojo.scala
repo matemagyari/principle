f297a5304483f24a06a0c2a677b7e59c92d8894e
magma <mate.magyari@leonteq.com>
2026-03-15 17:11:58 +0100

    simplify

--- src/main/scala/org/tindalos/principle/infrastructure/plugin/DesignQualityCheckerMojo.scala
+++ src/main/scala/org/tindalos/principle/infrastructure/plugin/DesignQualityCheckerMojo.scala
@@ -30,7 +30,7 @@ class DesignQualityCheckerMojo extends AbstractMojo {
 
     ReportsDirectoryManager.ensureReportsDirectoryExists()
 
-    val plan = ConstraintsReader.readFromFile(if (location == null) java.util.Optional.empty() else java.util.Optional.of(location))
+    val plan = ConstraintsReader.readFromFile(java.util.Optional.ofNullable(location))
 
     val analyse = PoorMansDIContainer.buildAnalyzer(plan.basePackage(), new LogPrinter(getLog()))
     try {
